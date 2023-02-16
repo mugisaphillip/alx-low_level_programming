7b7eae468255efbebff3bd6ae16c482237e4fed7
Phillip Mugisa <phillipmugisa@icloud.com>
2023-02-16 14:53:53 -0800

    C, Hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/**
+ * C Program that prints
+ * 'Programming is like building a multilingual puzzle'
+ * Author: Mugisa Phillip
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Programming is like building a multilingual puzzle\n");
+}
