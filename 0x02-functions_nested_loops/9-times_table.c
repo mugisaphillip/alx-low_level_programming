15f8c154b48232c515d59775d99af7aa928bff71
Phillip Mugisa <phillipmugisa@icloud.com>
2023-02-21 14:07:44 -0800

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,36 +1,38 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * times_table - program to print 9 time tables
- * Return: return
+ * times_table - print multiplication table
  */
 
 void times_table(void)
 {
-	int a, b, c, d, e;
+	int row;
+	int column;
+	int product;
 
-	for (a = 0; a <= 9; a++)
+	for (row = 0; row <= 9; row++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (column = 0; column <= 9; column++)
 		{
-			c = a * b;
-			if (c > 9)
+			product = (row * column);
+
+			if (column == 0)
 			{
-				d = c % 10;
-				e = (a - d) / 10;
-				_putchar(44);
-				_putchar(32);
-				_putchar(e + '0');
-			 	 _putchar(d + '0');
+				_putchar('0' + product);
 			}
-			else
+			else if (product <= 9)
 			{
-			if (b != 0)
-			{
-			_putchar(44);
-			_putchar(32);
-			_putchar(32);
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + product);
 			}
-			_putchar(c + '0');
+			else if (product > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
 			}
 		}
 		_putchar('\n');
