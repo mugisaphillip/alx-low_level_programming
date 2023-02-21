61352bdd32053f2ab00bb27f99efcb88098b1f42
Phillip Mugisa <phillipmugisa@icloud.com>
2023-02-21 14:36:23 -0800

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,21 +7,24 @@
 
 int main(void)
 {
-	int counter;
-	int countto = 50;
-	long a = 1;
-	long b = 2;
+    int n1 = 1;
+    int n2 = 2;
+    int next_term;
+    int i;
 
-	for (counter = 1; counter <= (countto / 2); counter++)
-	{
-		printf("%li %li ", a, b);
-		a += b;
-		b += a;
-	}
-	if (countto % 2 == 1)
-		printf("%li", a);
+    printf("%d, %d, ", n1, n2);  // print the first two terms
 
-	printf("\n");
+    for (i = 3; i <= 50; i++) {  // start the loop from 3rd term
+        next_term = n1 + n2;     // calculate the next term
+        printf("%d", next_term); // print the next term
+        if (i != 50) {           // add comma and space if it's not the last term
+            printf(", ");
+        }
+        n1 = n2;                 // update n1 and n2 for the next iteration
+        n2 = next_term;
+    }
 
-	return (0);
+    printf("\n");               // print a new line
+
+    return 0;
 }
