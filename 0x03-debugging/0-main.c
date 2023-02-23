652db2c53e9b44f3959207de50f83848026b4487
phillip mugisawq <phillipmugisa@icloud.com>
2023-02-23 05:45:46 +0300

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - tests function positive_or_negative()
- * return: 0
+ * main - test function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
