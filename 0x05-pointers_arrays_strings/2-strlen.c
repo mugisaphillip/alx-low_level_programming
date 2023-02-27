487f399cb9183a463774e3ba61ee850cf96f927a
phillip mugisawq <phillipmugisa@icloud.com>
2023-02-27 22:55:06 +0300

    pointers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,12 +2,14 @@
 
 /*
  * main - returns the length of a string. 
- * return - 0
+ * @s: string
+ * return - string length
  */
 
 int _strlen(char *s)
 {
 	int i;
+
 	for (i = 0; s[i] != '\0'; i++)
 		;
 
