e6ddbae224d551ab72873f570b13e491044a5dae
phillip mugisawq <phillipmugisa@icloud.com>
2023-03-20 22:43:37 +0300

    structs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifdef DOG_H
+#define DOG_H
+
+/*
+ * struct dog - struct for keeping dog dat
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+};
+
+#endif
