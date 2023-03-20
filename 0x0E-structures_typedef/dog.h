4605aa29007109647d4a0ad3de33b6d977e92e11
phillip mugisawq <phillipmugisa@icloud.com>
2023-03-20 22:48:17 +0300

    structs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,4 @@
-#ifdef DOG_H
+#ifndef DOG_H
 #define DOG_H
 
 /**
@@ -12,8 +12,20 @@
 struct dog
 {
 	char *name;
-	int age;
+	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
