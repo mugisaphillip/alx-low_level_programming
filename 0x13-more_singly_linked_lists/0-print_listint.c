56014d71272e5fdc2613fee00d792b6dd2861937
phillip mugisawq <phillipmugisa@icloud.com>
2023-04-03 22:55:59 +0300

    more sll

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,7 @@
 
 /**
  * print_listint - prints all elements of a listint_t list
- * @listint_t: pointer to start of list
+ * @h: pointer to start of list
  *
  * Return: number of nodes in the list
  */
