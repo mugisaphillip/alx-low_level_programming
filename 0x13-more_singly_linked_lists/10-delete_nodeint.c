bc23fd40fb34bfbe3b6cc408ab4571a3a23f4b78
phillip mugisawq <phillipmugisa@icloud.com>
2023-04-05 00:33:46 +0300

    more sll

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -6,7 +6,7 @@
  * @head: double pointer to list
  * @index: index at which to insert new node
  *
- * Return: 1 for success else -1
+ * Return: 1 (Success), of -1 (Fail)
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
@@ -16,7 +16,16 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (!head || !*head)
 		return (-1);
 
-	for (i = 0, temp = *head; temp; i++, temp = temp->next)
+	temp = *head;
+	if (index == 0)
+	{
+		*head = (temp)->next;
+		free(temp);
+		return (1);
+		
+	}
+
+	for (i = 0; temp; i++, temp = temp->next)
 	{
 		if (i + 1 == index)
 		{
