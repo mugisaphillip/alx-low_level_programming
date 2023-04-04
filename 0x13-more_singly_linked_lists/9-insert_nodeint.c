a87070ff4c217eb87213dcc66517af54113f193d
phillip mugisawq <phillipmugisa@icloud.com>
2023-04-05 00:13:48 +0300

    more sll

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,6 +22,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!new_node)
 		return (NULL);
 
+	new_node->n = n;
+	new_node->next = NULL;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		head = &new_node;
+		return (new_node);
+	}
+
 	temp = *head;
 	for (i = 0; temp; i++)
 	{
@@ -29,10 +38,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new_node->next = temp->next;
 			temp->next = new_node;
+			break;
 		}
 		temp = temp->next;
 	}
 
-	new_node->n = n;
 	return (new_node);
 }
