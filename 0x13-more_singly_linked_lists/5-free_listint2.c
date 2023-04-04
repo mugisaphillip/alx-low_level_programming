2c1b6c133a04744560355ee3486679aec89d5793
phillip mugisawq <phillipmugisa@icloud.com>
2023-04-04 22:48:04 +0300

    more sll

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - free a list
+ * @head: double pointer to list
+ *
+ * Return: Always void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *next;
+
+	if (*head == NULL)
+		return;
+
+	current = *head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head = NULL;
+}
