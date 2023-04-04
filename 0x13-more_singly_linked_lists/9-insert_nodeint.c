7a2497cd6e89c6089bdece8bca2607028f2c7099
phillip mugisawq <phillipmugisa@icloud.com>
2023-04-05 00:18:31 +0300

    more sll

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,19 +15,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *temp;
 	unsigned int i;
 
-	if (!head || !*head)
-		return (NULL);
-
 	new_node = malloc(sizeof(listint_t));
-	if (!new_node)
+	if (!new_node || !head)
 		return (NULL);
 
 	new_node->n = n;
 	new_node->next = NULL;
+
 	if (idx == 0)
 	{
 		new_node->next = *head;
-		head = &new_node;
+		*head = new_node;
 		return (new_node);
 	}
 
