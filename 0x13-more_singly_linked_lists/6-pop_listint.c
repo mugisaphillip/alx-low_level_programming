2e6857ffee983a8e117b35e94888e1c2b76d42bb
phillip mugisawq <phillipmugisa@icloud.com>
2023-04-04 23:21:25 +0300

    more sll

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,12 +10,14 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
+	int num;
 
 	if (*head == NULL)
 		return (0);
 
 	temp = *head;
-	*head = (*head)->next;
+	num = temp->n;
+	*head = temp->next;
 	free(temp);
-	return ((*head)->n);
+	return (num);
 }
