97695c5a4129015e740169f28a1fc674c28f2391
phillip mugisawq <phillipmugisa@icloud.com>
2023-04-11 00:00:08 +0300

    I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 		;
 
 	n_written = write(fd, text_content, len);
-	if (n_written < 1)
+	if (n_written == -1)
 		return (-1);
 
 	close(fd);
