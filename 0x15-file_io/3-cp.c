346b44c5c8c5b3f671b6e4a915ab8f88cb581485
phillip mugisawq <phillipmugisa@icloud.com>
2023-04-11 00:39:33 +0300

    I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,8 +19,9 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1) dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", argv[1]), exit(98);
-	
+	if (fd_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", argv[1]), exit(98);
+
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
 	if (fd_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
