aa2b3f3dc0311648f5b15cb71cf3388426c71fed
phillip mugisawq <phillipmugisa@icloud.com>
2023-04-11 00:36:15 +0300

    I/O

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * main - copies content of files
+ * @argc: argument count
+ * @argv: argument vector
+ * ReturnL 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, read_stat, write_stat;
+	mode_t perm = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
+	char buffer[BUFSIZE];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1) dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", argv[1]), exit(98);
+	
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
+	if (fd_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+
+	read_stat = 1;
+	while (read_stat)
+	{
+		read_stat = read(fd_from, buffer, BUFSIZE);
+		if (read_stat == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		if (read_stat > 0)
+		{
+			write_stat = write(fd_to, buffer, read_stat);
+			if (write_stat != read_stat || write_stat == -1)
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		}
+	}
+	if (close(fd_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+
+	if (close(fd_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+
+	return (0);
+}
